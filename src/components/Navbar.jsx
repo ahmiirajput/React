4e35a0fc27382014518a7e601095f72b0a099a91
Ahmad Rajpoot <ahmadrajpoot3040319651@gmail.com>
2025-01-01 13:19:37 +0500

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <div>
+            
+            <ul>
+                <li>Home</li>
+                <li>Servicese</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
